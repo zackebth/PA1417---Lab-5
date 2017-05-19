269d80d88b5446aba7c2173fb64fc2e589a7bbf9
Zacke Madsen <zacke_madsen@live.se>
2017-05-19 21:04:11 +0200

    added scoring system

--- src/bowling/Game.java
+++ src/bowling/Game.java
@@ -1,5 +1,16 @@
 package bowling;
 
 public class Game {
-
+	
+	private int score = 0;
+	
+	public void roll(int pinsDown) {
+		score += pinsDown;
+	}
+	
+	public int score() {
+		
+		return score;
+	}
+	
 }
--- src/bowling/GameTest.java
+++ src/bowling/GameTest.java
@@ -7,8 +7,27 @@ import org.junit.Test;
 public class GameTest {
 
 	@Test
-	public void MakeGameTest() {
-		new Game();
+	public void MakeRollBallTest() {
+		Game game = new Game();
+		game.roll(0);
+	}
+	
+	@Test
+	public void MakeGutterGameTest() {
+		Game game = new Game();
+		for (int i = 0; i < 20; i++) {
+			game.roll(0);
+		}
+		assertEquals(0, game.score());
 	}
 
+	
+	@Test
+	public void MakeScoreOfOnes() {
+		Game game = new Game();
+		for (int i = 0; i < 20; i++) {
+			game.roll(1);
+		}
+		assertEquals(20, game.score());
+	}
 }
