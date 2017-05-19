4f1fd808c084b7d547a960bc7c5b418a7878fb46
Zacke Madsen <zacke_madsen@live.se>
2017-05-19 22:07:57 +0200

    Test case for user story 6

--- src/bowling/Game.java
+++ src/bowling/Game.java
@@ -1,16 +1,37 @@
 package bowling;
 
 public class Game {
-	
-	private int score = 0;
+		
+	private int roll = 0;
+	private int[] rolls = new int[21];
+
+	public void roll(int...rolls) {
+		for (int pinsDown : rolls) {
+			roll(pinsDown);
+		}
+	}
 	
 	public void roll(int pinsDown) {
-		score += pinsDown;
+		rolls[roll++] = pinsDown;
 	}
 	
 	public int score() {
-		
+		int score = 0;
+		int cursor = 0;
+		for (int frame = 0; frame < 10; frame++) {
+			if (isSpare(cursor)){
+				score += 10 + rolls[cursor+2];
+				cursor += 2;
+			} else {
+				score += rolls[cursor] + rolls[cursor+1];
+				cursor += 2;
+			}
+		}
 		return score;
+	}
+
+	private boolean isSpare(int cursor) {
+		return rolls[cursor] + rolls[cursor+1] == 10;
 	}
 	
 }
--- src/bowling/GameTest.java
+++ src/bowling/GameTest.java
@@ -1,33 +1,48 @@
 package bowling;
 
-import static org.junit.Assert.*;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import org.junit.Before;
 import org.junit.Test;
+
 
 public class GameTest {
 
-	@Test
-	public void MakeRollBallTest() {
-		Game game = new Game();
-		game.roll(0);
+	private Game game;
+	
+	@Before
+	public void setUp() {
+		game = new Game();
 	}
+
 	
 	@Test
 	public void MakeGutterGameTest() {
-		Game game = new Game();
-		for (int i = 0; i < 20; i++) {
-			game.roll(0);
-		}
-		assertEquals(0, game.score());
+		game.roll(0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+		assertThat(game.score(), is(0));
 	}
 
 	
 	@Test
 	public void MakeScoreOfOnes() {
-		Game game = new Game();
-		for (int i = 0; i < 20; i++) {
-			game.roll(1);
-		}
-		assertEquals(20, game.score());
+		game.roll(1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1);
+		assertThat(game.score(), is(20));
 	}
+
+	/*User story 6 START, recgognize spare and game with spare*/
+	@Test
+	public void MakeSpareTest() {
+		game.roll(5,5, 3,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+		assertThat(game.score(), is(16));
+	}
+	
+	@Test
+	public void MakeGameWithSpare() {
+		game.roll(1,9, 3,6, 7,2, 3,6, 4,4, 5,3, 3,3, 4,5, 8,1, 2,6);
+		assertThat(game.score(), is(88));
+	}
+	/*User story 6 END*/
+	
 }
